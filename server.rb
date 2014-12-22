9c88b9763c33de77be9b60067c6f8a95a08b7a65
Tovarm <tovamosk@gmail.com>
2014-12-22 15:54:39 -0500

    added subscription page

--- server.rb
+++ server.rb
@@ -6,22 +6,6 @@ require './db/connection.rb'
 require './lib/class_wiki.rb'
 
 
-get '/' do
-	Mustache.render(File.read('./views/index.html'), params.to_a)
-end
-
-get '/new_entry' do
-	File.read('./views/new_entry.html')
-end
-
-
-# get '/entries_all' do
-# 	all_entries = Entry.all
-# 	Mustache.render(File.read './show_entry_page'), {entry: all_entries}.to_a)
-# end
-
-# show a specific entry
-
 post '/search_results' do
 	"You searched for: #{params["search"]}"
 	# entry = Entry.all.to_a
@@ -34,6 +18,32 @@ post '/search_results' do
 			return "Sorry, no matches found"
 		end
 end
+
+get '/' do
+	Mustache.render(File.read('./views/index.html'), params.to_a)
+end
+
+get '/new_entry' do
+	File.read('./views/new_entry.html')
+end
+
+post '/new_entry' do
+	Entry.create(entry_title: params["entry_title"], entry_content: params["entry_content"], author_id: params[
+		"author"])
+	Mustache.render(File.read('./views/confirm_entry.html'), params.to_a)
+end
+
+
+get '/subscribe' do
+	File.read('./views/subscribe.html')
+end
+
+post '/confirm_subscription' do
+	Subscriber.create(name: params["name"], email: params["email"], phone: params["phone"])
+	Mustache.render(File.read('./views/confirm_subscription.html'), params.as_json)
+end
+
+
 get '/new_author' do
 	File.read('./views/new_author.html')
 end
@@ -43,16 +53,17 @@ post '/new_author' do
 	Mustache.render(File.read('./views/confirm_author.html'), params.as_json)
 end
 
-post '/new_entry' do
-	Entry.create(entry_title: params["entry_ title"], entry_content: params["entry_content"], author_id: params[
-		"author"])
-	Mustache.render(File.read('./views/confirm_entry.html'), params.as_json)
-end
 
+# show a specific entry
 get '/entry/:id' do
-	entry = Entry.find_by id: params["id"]
+
+	Entry.find_by entry_id: params["entry_id"]
 	Mustache.render(File.read('./views/show_entry_page.html'), params.as_json)
 end
 
+# get '/entries_all' do
+# 	all_entries = Entry.all
+# 	Mustache.render(File.read './show_entry_page'), {entry: all_entries}.to_a)
+# end
 
 
