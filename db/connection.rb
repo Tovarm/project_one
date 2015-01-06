77ca5b4113efff74cd273c2a4ddb22d396d4e7f3
Tovarm <tovamosk@gmail.com>
2015-01-06 11:21:37 -0500

    updated connection.rb file

--- db/connection.rb
+++ db/connection.rb
@@ -1,12 +1,13 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection({
-  :adapter => "postgresql",
-  :host => "localhost",
-  :username => "Tova",
-  :database => "wiki"
-  })
+# ActiveRecord::Base.establish_connection({
+#   :adapter => "postgresql",
+#   :host => "localhost",
+#   :username => "Tova",
+#   :database => "wiki"
+#   })
+  
+  ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"]  + '@127.0.0.1/<wiki>')
 
   ActiveRecord::Base.logger = Logger.new(STDOUT) # <-- optional debugging tool
 
-  
